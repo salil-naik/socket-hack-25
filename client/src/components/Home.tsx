e10a14a90a72530cfb68aa4b4fbea8a44a4fe384
salil-naik <salil.naik27@gmail.com>
2025-02-23 14:10:49 +0400

    calling status on button click

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,19 +1,25 @@
-import useCheckout from "../hooks/useCheckout";
+import { useState } from "react";
+import useCheckStatus from "../hooks/useCheckStatus";
+
 
 export const Home = () => {
-  const { postCheckout } = useCheckout();
-  const itemsToBuy = [
-    { id: 1, quantity: 2 },
-    { id: 2, quantity: 1 },
-  ];
+  // const { postCheckout } = useCheckout();
+  const [sessionId, setSessionId] = useState<string>();
+  const { status, loading } = useCheckStatus(sessionId || "");
+  // const itemsToBuy = [
+  //   { id: 1, quantity: 2 },
+  //   { id: 2, quantity: 1 },
+  // ];
 
   return (
     <div>
       <h1>Pay with Socket</h1>
       <div className="card">
-        <button onClick={() => postCheckout(itemsToBuy)}>
+        <button onClick={() => setSessionId("cs_test_b1XdsFFukLhq7w9JwUW1DkQm0LsRA3GTqPbDSW672XP4TQp4JsX9LkobtD")}>
           Pay motherfucker!
         </button>
+        {loading && <span>loading</span>}
+        {status && <span>{status}</span>}
       </div>
       {/* <p className="read-the-docs">
         10% of every transaction goes toward keeping a roof over my head.
